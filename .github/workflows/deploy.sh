b99646766e83d4f2f6af1dae9997024d10add028
Andris Reinman <andris@reinman.eu>
2022-10-23 19:22:54 +0300

    added deploy action

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -3,7 +3,7 @@ on:
         branches:
             - master
 
-name: Deploy test instance
+name: Deploy instance
 
 jobs:
     deploy:
@@ -22,14 +22,17 @@ jobs:
 
             - name: Deploy to server
               env:
-                  TARGET_HOST_01: srv-01.srv.dev
+                  TARGET_HOST_01: dns-01.emailengine.app
+                  TARGET_HOST_02: dns-02.emailengine.app
                   NODE_ENV: production
-                  SERVICE_NAME: ethereal-msa
+                  SERVICE_NAME: pending-dns
               id: deploy
               run: |
                   echo $GITHUB_SHA > commit.txt
                   npm install --production
                   tar czf /tmp/${SERVICE_NAME}.tar.gz --exclude .git .
                   scp /tmp/${SERVICE_NAME}.tar.gz deploy@${TARGET_HOST_01}:
+                  scp /tmp/${SERVICE_NAME}.tar.gz deploy@${TARGET_HOST_02}:
                   ssh deploy@$TARGET_HOST_01 "/opt/deploy.sh ${SERVICE_NAME}"
+                  ssh deploy@$TARGET_HOST_02 "/opt/deploy.sh ${SERVICE_NAME}"
 
